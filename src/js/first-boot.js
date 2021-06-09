63d1cd7fa02895ec9f6e8081006ed308c06296ef
KorbsStudio <korbsstudio@github.com>
2021-06-09 18:44:42 -0400

    Reset First Session

--- src/js/first-boot.js
+++ src/js/first-boot.js
@@ -1,7 +1,28 @@
-if(!localStorage.getItem("firstTime")){
-  console.log('New user, showing welcome screen') // This will show trigger if you set cache or remove session cookies
-  window.open('https://software.falixnodes.net/welcome/', 'width=500,height=300frame=true,nodeIntegration=no');
-  localStorage.setItem("firstTime","true");
+if(!localStorage.getItem("firstBoot")){
+  console.log('New user, showing welcome screen') // This will show up if your cache was clear
+  localStorage.setItem("firstBoot","true");
+  setTimeout(() => {
+    createBrowserWindow();
+  }, 5250);
 }else{
-  console.log('Existing user, aborting welcome screen.')
+  console.log('Existing user, aborting welcome screen.');
+}
+
+function createBrowserWindow() {
+  const remote = require('electron').remote;
+  const BrowserWindow = remote.BrowserWindow;
+  const welcome = new BrowserWindow({
+    height: 360,
+    width: 500,
+    autoHideMenuBar: true,
+    resizable: false,
+    maxHeight: false,
+    center: true,
+    webPreferences: {
+      devTools: true
+    }
+  });
+  welcome.loadFile('./src/welcome.html');
+  mainWindow.hide;
+  setTimeout(() => {mainWindow.hide();}, 5500);
 }
