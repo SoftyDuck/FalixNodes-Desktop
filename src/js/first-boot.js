fecb1cdfb0978a2054af9a933c2e46a91cafbb74
KorbsStudio <KorbsStudio@OnMail.com>
2021-07-10 15:48:13 -0400

    Highlight selected options on welcome screen

--- src/js/first-boot.js
+++ src/js/first-boot.js
@@ -1,9 +1,8 @@
 if(!localStorage.getItem("firstBoot")){
   console.log('New user, showing welcome screen') // This will show up if your cache was clear or the item was removed from local storage
-  localStorage.setItem("firstBoot","true");
   setTimeout(() => {
     document.write('<style>.content {opacity: 0.4; background: rgb(11 23 37 / 80%) !important;} div#window-controls, div#game_panel, div#client_panel, div#help_center, div#support, div#terminal_mining, div#settings, div#sb_ct {display: none;}</style> <div include-html="./index.html"></div> <div include-html="./welcome.html"></div> <script src="./js/ui/include.js"></script> <script>includeHTML();</script> <script src="./js/switchIndex.js"></script>')
-  }, 5250);
+  }, 1000);
 }else{
   console.log('Existing user, aborting welcome screen.');
 }
