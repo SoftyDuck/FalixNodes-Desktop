4be4e5ef6ffe72777728467003420d539516613f
KorbsStudio <KorbsStudio@OnMail.com>
2021-08-02 17:23:21 -0400

    Remove unused code

--- src/js/addMenu.js
+++
@@ -1,8 +0,0 @@
-function openAddMenu() {
-    var x = document.getElementById("new_window_options");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
