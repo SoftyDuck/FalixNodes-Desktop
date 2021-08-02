5694d9cefce065d58d217e414b749bfba851aea1
KorbsStudio <KorbsStudio@OnMail.com>
2021-08-02 17:24:50 -0400

    Update controls

--- src/js/electron/preload.js
+++ src/js/electron/preload.js
@@ -1,68 +1,12 @@
 const remote = require('electron').remote;
-
-const win = remote.getCurrentWindow();
-
-window.addEventListener("DOMContentLoaded", () => {
-    handleWindowControls();
-});
-
-window.onbeforeunload = (event) => {
-    win.removeAllListeners();
-}
-
+const mainWindow = remote.getCurrentWindow();
+window.addEventListener("DOMContentLoaded", () => {handleWindowControls();});
+window.onbeforeunload = (event) => {mainWindow.removeAllListeners();}
 function handleWindowControls() {
-    document.getElementById('min-button').addEventListener("click", event => {
-        win.minimize();
-    });
-
-    document.getElementById('max-button').addEventListener("click", event => {
-        win.maximize();
-    });
-
-    document.getElementById('restore-button').addEventListener("click", event => {
-        win.unmaximize();
-    });
-
-    document.getElementById('close-button').addEventListener("click", event => {
-        win.close();
-    });
-
-    toggleMaxRestoreButtons();
-    win.on('maximize', toggleMaxRestoreButtons);
-    win.on('unmaximize', toggleMaxRestoreButtons);
-
-    function toggleMaxRestoreButtons() {
-        if (win.isMaximized()) {
-            document.body.classList.add('maximized');
-        } else {
-            document.body.classList.remove('maximized');
-        }
-    }
+    document.getElementById('min-button').addEventListener("click", event => {mainWindow.minimize();});
+    document.getElementById('max-button').addEventListener("click", event => {mainWindow.maximize();});
+    document.getElementById('restore-button').addEventListener("click", event => {mainWindow.unmaximize();});
+    document.getElementById('close-button').addEventListener("click", event => {mainWindow.close();});
 }
-
-console.log('preload.js file is loaded.')
 delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
-
-
-function downloadStartedNotify(){
-    const notif={
-          title: 'Download Started',
-          body: 'Download file...',
-        };
-    new Notification(notif).show();
-  }
-  function downloadSuccessNotify(){
-    const notif={
-          title: 'Download Successful',
-          body: 'The file has been downloaded.',
-        };
-    new Notification(notif).show();
-  }
-  function downloadFailedNotify(){
-    const notif={
-          title: 'Download Failed',
-          body: `${state}`,
-        };
-    new Notification(notif).show();
-  }
