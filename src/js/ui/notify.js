9034f1da7935fb910b50d0c61412e9607d989cd1
KorbsStudio <KorbsStudio@OnMail.com>
2021-08-22 15:26:56 -0400

    Add new push notification system

---
+++ src/js/ui/notify.js
@@ -0,0 +1,6 @@
+function openInBrowser() {
+    document.getElementById('nd_openInBrowser').style.display = 'inherit';
+    setTimeout (() => {
+        document.getElementById('nd_openInBrowser').style.display = 'none';
+    }, 2500)
+}
