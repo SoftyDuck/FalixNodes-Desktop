c4d98d89cd3faf2e32397f56da0c88beb4fa8e17
Korbs <KorbsStudio@OnMail.com>
2021-08-25 21:04:27 -0400

    Add new tray

---
+++ src/tray/menu.js
@@ -0,0 +1,17 @@
+function switchTab(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("page");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("menu");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+setTimeout(() => {
+  document.getElementById("default_select").click();
+}, 0);
