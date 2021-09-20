2affe3672de51a7accfbdcccfb4232f22f75ecc4
Korbs <KorbsStudio@OnMail.com>
2021-09-19 21:38:47 -0400

    Move "live server" area

--- src/js/electron/public/detect.js
+++
@@ -1,9 +0,0 @@
-var OSVersion = "Not Supported";
-if (window.navigator.userAgent.indexOf("Windows NT 11.0")!= -1) OSVersion="Windows 11";
-if (window.navigator.userAgent.indexOf("Windows NT 10.0")!= -1) OSVersion="Windows 10";
-if (window.navigator.userAgent.indexOf("Windows NT 6.3") != -1) OSVersion="Windows 8.1";
-if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSVersion="Windows 8";
-if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSVersion="Windows 7";
-if (window.navigator.userAgent.indexOf("Linux") != -1) OSVersion="Linux";
-if (window.navigator.userAgent.indexOf("Macintosh") != -1) OSVersion="macOS";
-if (window.navigator.userAgent.indexOf("MacIntel") != -1) OSVersion="macOS";
--- src/js/electron/server.js
+++ src/js/electron/server.js
@@ -1,5 +1,5 @@
 var static = require('node-static');
-var file = new static.Server(`${__dirname}/public`)
+var file = new static.Server(`${__dirname}../../../../src/html/server/`)
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
