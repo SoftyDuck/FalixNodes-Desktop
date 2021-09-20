85e6cca339d6fa7796133842aab675ce89552554
Korbs <KorbsStudio@OnMail.com>
2021-09-20 16:42:14 -0400

    Remove extra comments and old code

--- src/js/electron/preload.js
+++ src/js/electron/preload.js
@@ -1,11 +1,8 @@
 const { contextBridge, ipcRenderer} = require("electron");
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
   "api", {
       send: (channel, data) => {
-          // whitelist channels
           let validChannels = ["minimize",
                                "maximize",
                                "restore",
