bb6bd4101f486e45d50ee2f5402d2eb647c2ef0f
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2022-01-09 01:52:16 -0500

    Rename "Include" function to "Insert" and set HTML 'include' pages as components

--- src/js/ui/insert.js
+++ src/js/ui/insert.js
@@ -1,17 +1,18 @@
-function includeHTML() {
+function insert() {
     var z, i, elmnt, file, xhttp;
     z = document.getElementsByTagName("*");
         for (i = 0; i < z.length; i++) {
             elmnt = z[i];
-            file = elmnt.getAttribute("include-html");
+            file = elmnt.getAttribute("insert");
             if (file) {
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4) {
                 if (this.status == 200) {elmnt.innerHTML = this.responseText;}
                 if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                elmnt.removeAttribute("include-html");
-                includeHTML();
+                if (this.status == 503) {elmnt.innerHTML = "Timed out.";}
+                elmnt.removeAttribute("insert");
+                insert();
                 }
             }
             xhttp.open("GET", file, true);
