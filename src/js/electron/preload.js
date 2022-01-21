17c0935167ac51a24398573d8f8600dfe280d0fc
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2022-01-21 00:52:49 -0500

    Add SFTP Client

--- src/js/electron/preload.js
+++ src/js/electron/preload.js
@@ -16,7 +16,23 @@ contextBridge.exposeInMainWorld( "api", { send: (channel, data) => {let validCha
   "btTP",
   "btAY",
   "btVB",
-  "open-glasstron-api-demo"
+  "open-glasstron-api-demo",
+  "display_file_properties",
+  "change_local_dir",
+  "get_cur_local_dir",
+  "get_cur_remote_dir",
+  "get_remote_dir_list",
+  "download_remote_files",
+  "upload_local_files",
+  "copy_local_files",
+  "cut_local_files",
+  "copy_remote_files",
+  "cut_remote_files",
+  "paste_local_files",
+  "paste_remote_files",
+  "make_local_dir",
+  "make_remote_dir",
+  "openSFTP"
 ]; if (validChannels.includes(channel)) {ipcRenderer.send(channel, data);}}});
 
 delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
