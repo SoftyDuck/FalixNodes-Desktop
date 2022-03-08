71903a45da48727f2b6e1b4c9bbc12f75fd7f2fe
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2022-03-07 23:10:23 -0500

    Add `https://dev-panel.falixnodes.net/`

--- src/js/detection/domain-restrictions/game-panel.js
+++ src/js/detection/domain-restrictions/game-panel.js
@@ -1,5 +1,6 @@
 var url = window.location.href;
 if(url.startsWith("https://panel.falixnodes.net/")
+|| url.startsWith("https://dev-panel.falixnodes.net")
 || url.startsWith("https://discord.com/")
 )
 {} else {setTimeout(() => {location.href = 'https://panel.falixnodes.net/'}, 250);}
