519e3cf4f57ee4c4134282dcabaa5f0d3c00a37b
KorbsStudio <51213244+KorbsStudio@users.noreply.github.com>
2022-04-04 20:37:18 -0400

    Remove custom SFTP junk

--- src/assets/scripts/electronjs/sftp.js
+++
@@ -1,3 +0,0 @@
-const { ipcRenderer} = require("electron");
-delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
-process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
--- src/components/content/SFTP/renderer/console.js
+++
@@ -1,9 +0,0 @@
-exports.writeToConsole = function(message){
-  document.getElementById('console_output').innerHTML += ('<br>' + '> ' + message);
-  document.getElementById('console_output').scrollTop = document.getElementById('console_output').scrollHeight;
-}
-
-function writeToConsole(message){
-  document.getElementById('console_output').innerHTML += ('<br>' + '> ' + message);
-  document.getElementById('console_output').scrollTop = document.getElementById('console_output').scrollHeight;
-}
