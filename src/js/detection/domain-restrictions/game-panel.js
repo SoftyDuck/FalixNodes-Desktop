833f8940ebd009e62cde765ecadb8652ba6035bb
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2022-04-22 21:35:38 -0400

    update

--- src/js/detection/domain-restrictions/game-panel.js
+++ src/js/detection/domain-restrictions/game-panel.js
@@ -1,6 +1,7 @@
 var url = window.location.href;
 if(url.startsWith("https://panel.falixnodes.net/")
 || url.startsWith("https://dev-panel.falixnodes.net")
+|| url.startsWith("https://prem-panel.falixnodes.net")
 || url.startsWith("https://discord.com/")
 )
 {} else {setTimeout(() => {location.href = 'https://panel.falixnodes.net/'}, 250);}
