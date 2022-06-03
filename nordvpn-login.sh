1fd339cdee50e4e97be4aa2c49bd1a4895d949ec
Korbs <ks@korbsstudio.com>
2022-06-03 04:33:38 -0400

    update

---
+++ nordvpn-login.sh
@@ -0,0 +1,13 @@
+zenity --ok-label="Login to NordVPN" --cancel-label="Cancel" --question --text "NordVPN Login is required. Let's get you logged in!"
+EMAIL=$( zenity --entry --text="Type in your NordVPN Email" )
+PASSWORD=$( zenity --password="Type in your NordVPN Password" )
+
+if [ $? = 0 ]
+then
+    nordvpn login --legacy --username $EMAIL --password $PASSWORD
+else
+    echo "User exit"
+fi
+
+# Zenity is Linux only, it is used since most distros(Really all of them) come with Zenity pre-installed.
+# This is used if the user is not logged into their NordVPN account, the VPN area of FalixNodes Desktop uses NordVPN commands, therefore NordVPN is required.
