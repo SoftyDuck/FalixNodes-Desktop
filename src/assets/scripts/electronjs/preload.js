646dfb980082b694af59fdbad1264a3fec5e1b26
Korbs <ks@korbsstudio.com>
2022-06-03 14:03:27 -0400

    Update preload.js

--- src/assets/scripts/electronjs/preload.js
+++ src/assets/scripts/electronjs/preload.js
@@ -5,7 +5,8 @@ contextBridge.exposeInMainWorld( "api", { send: (channel, data) => {let validCha
 "logout",
 "relaunch",
 "enableVPN",
-"disableVPN"
+"disableVPN",
+"loginVPN"
 ]; if (validChannels.includes(channel)) {ipcRenderer.send(channel, data);}}});
 delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
