a59472e88d622d4b3833e46f7a47b841fb186624
Korbs <ks@korbsstudio.com>
2022-06-08 16:15:21 -0400

    Fix Glance feature

--- src/assets/scripts/webviews/glance-servers.js
+++ src/assets/scripts/webviews/glance-servers.js
@@ -1,6 +1,8 @@
 var url = window.location.href;
 
-if(url.endsWith("/auth/login/"))
-{setTimeout(() => {
-    document.getElementById("app").innerHTML = '<p id="falix-desktop-glance">Server panel login is required to view your servers. <a style="text-decoration: underline" href="https://panel.falixnodes.net/">Reload Componment</a></p><style>#falix-desktop-glance {display: inherit}</style>';
-}, 250);} else {}
+setTimeout(() => {
+    if(url.match("https://dev-panel.falixnodes.net/auth/login"))
+    {setTimeout(() => {
+        document.getElementById("app").innerHTML = '<p id="falix-desktop-glance">Server panel login is required to view your servers. <a style="text-decoration: underline" href="https://panel.falixnodes.net/">Reload Componment</a></p><style>#falix-desktop-glance {display: inherit}</style>';
+    }, 1000);} else {console.log('null')}
+}, 6000);
