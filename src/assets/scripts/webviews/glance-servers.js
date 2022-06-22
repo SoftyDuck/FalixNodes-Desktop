a2687697375b7968a562c3bc49bd5a4b087173c1
Korbs <ks@korbsstudio.com>
2022-06-22 17:26:25 -0400

    Fix login error for At a Glance feature on the Dashboard

--- src/assets/scripts/webviews/glance-servers.js
+++ src/assets/scripts/webviews/glance-servers.js
@@ -1,8 +1,8 @@
 var url = window.location.href;
 
 setTimeout(() => {
-    if(url.match("https://dev-panel.falixnodes.net/auth/login"))
+    if(window.location.href.match("https://dev-panel.falixnodes.net/auth/login"))
     {setTimeout(() => {
-        document.getElementById("app").innerHTML = '<p id="falix-desktop-glance">Server panel login is required to view your servers. <a style="text-decoration: underline" href="https://panel.falixnodes.net/">Reload Componment</a></p><style>#falix-desktop-glance {display: inherit}</style>';
+        document.getElementById("app").innerHTML = '<p id="falix-desktop-glance">Server panel login is required to view your servers. <a style="text-decoration: underline; cursor: pointer;" href="https://panel.falixnodes.net/">Reload Componment</a></p><style>#falix-desktop-glance {display: inherit; pointer-events: all;}</style>';
     }, 1000);} else {console.log('null')}
-}, 6000);
+}, 0500);
