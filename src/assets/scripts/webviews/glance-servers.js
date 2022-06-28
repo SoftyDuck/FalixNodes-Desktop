19e6791893f3a4858e6821eb8f526588b1fd7d13
Korbs <ks@korbsstudio.com>
2022-06-28 15:52:54 -0400

    Update CSS for Glance

--- src/assets/scripts/webviews/glance-servers.js
+++ src/assets/scripts/webviews/glance-servers.js
@@ -1,6 +1,9 @@
 var url = window.location.href;
 
 setTimeout(() => {
+    document.querySelectorAll('style,link[rel="stylesheet"]')
+    .forEach(element => element.remove())
+
     if(window.location.href.match("https://dev-panel.falixnodes.net/auth/login"))
     {setTimeout(() => {
         document.getElementById("app").innerHTML = '<p id="falix-desktop-glance">Server panel login is required to view your servers. <a style="text-decoration: underline; cursor: pointer;" href="https://panel.falixnodes.net/">Reload Componment</a></p><style>#falix-desktop-glance {display: inherit; pointer-events: all;}</style>';
