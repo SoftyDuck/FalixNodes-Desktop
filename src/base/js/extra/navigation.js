f05b37348246b8b475896cd5b4c552bea95e9d70
Korbs <ks@korbsstudio.com>
2022-08-11 01:24:27 -0400

    Add navigation script for modals and remove left over code

--- src/base/js/extra/navigation.js
+++ src/base/js/extra/navigation.js
@@ -12,33 +12,31 @@ function toTab(pageName,elmnt,color) {
     elmnt.style.backgroundColor = color;
 }
 
+function toggleModal(pageName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("modal");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(pageName).style.display = "inherit";
+}
+
+function toSettingsTab(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("hs-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("modal-sidebar-tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "inherit";
+  elmnt.style.backgroundColor = color;
+}
+
 do {
   document.querySelector("body > sidebar > div > div.sidebar-top > button:nth-child(1)").click();
+  document.querySelector("#settings > div.modal-sidebar > div.modal-sidebar-top > div:nth-child(1)").click();
 }
 while (addComponent());
-
-
-function toNotifications() {
-  document.querySelector('.tabs#dashboard').style.display = 'none'
-  document.querySelector('.tabs#notifications').style.display = 'block'
-  document.querySelector('.tabs#settings').style.display = 'none'
-}
-function toSettings() {
-  document.querySelector('.tabs#dashboard').style.display = 'none'
-  document.querySelector('.tabs#notifications').style.display = 'none'
-  document.querySelector('.tabs#settings').style.display = 'grid'
-}
-function toDashboard() {
-  document.querySelector('.tabs#dashboard').style.display = 'grid'
-  document.querySelector('.tabs#notifications').style.display = 'none'
-  document.querySelector('.tabs#settings').style.display = 'none'
-}
-
-function tgMH() {
-  document.querySelector('button#command-menu-o').style.display = 'none';
-  document.querySelector('button#profile').style.display = 'none';
-}
-function tgMS() {
-  document.querySelector('button#command-menu-o').style.display = 'inherit';
-  document.querySelector('button#profile').style.display = 'inherit';
-}
