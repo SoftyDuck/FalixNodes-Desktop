2f7d0ce5afc0d746b58976e17f0705b6e8a21422
Korbs <ks@korbsstudio.com>
2022-08-11 01:25:22 -0400

    Remove `v8-compile-cache` and enable `webviewTag`

--- src/process/index.js
+++ src/process/index.js
@@ -1,18 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 const glasstron = require('glasstron')
-require('v8-compile-cache');
 
 const createMainWindow = () => {
     let primaryWindow = new glasstron.BrowserWindow({
         width: 1200,
         height: 800,
         frame: true,
+        autoHideMenuBar: true,
         blur: true,
         blurType: 'blurbehind',
-        webPrefernces: {
-            webviewTag: true,
-            contextIsolation: true,
-            nodeIntegration: false
+        webPreferences: {
+            webviewTag: true
         }
     })
     primaryWindow.loadFile('./src/index.html')
