dde1a5c2f929f5b52b99a484c3bf64d8b79193b1
Korbs <ks@korbsstudio.com>
2022-08-12 04:29:25 -0400

    Update index.js

--- src/process/index.js
+++ src/process/index.js
@@ -28,24 +28,24 @@ function resetSystemHostFile() {
   else if(process.platform == 'win32'){
     console.log('Resetting Host File');
     let ps = new PowerShell(`
-    cp C:\Windows\System32\drivers\etc\hosts C:\Windows\System32\drivers\etc\hosts_backup
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/FalixNodes-Software/FalixNodes-Desktop/directive-rewrite-of-v4/src/base/etc/hosts" -OutFile "C:\Windows\System32\drivers\etc\hosts"
-    powershell (New-Object -ComObject Wscript.Shell).Popup("Host file has been resetted.",0,"FalixNodes Desktop - Troubleshooting",0x0)
-    powershell (New-Object -ComObject Wscript.Shell).Popup("Original hosts file was backed up to your Documents.",0,"FalixNodes Desktop - Troubleshooting",0x0)
+      cp C:\Windows\System32\drivers\etc\hosts C:\Windows\System32\drivers\etc\hosts_backup
+      Invoke-WebRequest -Uri "https://raw.githubusercontent.com/FalixNodes-Software/FalixNodes-Desktop/directive-rewrite-of-v4/src/base/etc/hosts" -OutFile "C:\Windows\System32\drivers\etc\hosts"
+      powershell (New-Object -ComObject Wscript.Shell).Popup("Host file has been resetted.",0,"FalixNodes Desktop - Troubleshooting",0x0)
+      powershell (New-Object -ComObject Wscript.Shell).Popup("Original hosts file was backed up to your Documents.",0,"FalixNodes Desktop - Troubleshooting",0x0)
   `)
   }
   else{
     console.log('Resetting Host File');
     exec(`
-    PASSWD="$(zenity --password --title=Authentication Required)\n"
-    echo -e $PASSWD | sudo -S \
-    sudo cp /etc/hosts /etc/hosts_backup
-    cd /etc/
-    sudo rm hosts
-    
-    (sudo wget https://raw.githubusercontent.com/FalixNodes-Software/FalixNodes-Desktop/directive-rewrite-of-v4/src/base/etc/hosts) | zenity --progress --title="FalixNodes Desktop - Troubleshooting" --text="Downloading default hosts file..." --pulsate --width="500"
-    
-    zenity --info --text="Original hosts file was backed up to your Documents."
+      PASSWD="$(zenity --password --title=Authentication Required)\n"
+      echo -e $PASSWD | sudo -S \
+      sudo cp /etc/hosts /etc/hosts_backup
+      cd /etc/
+      sudo rm hosts
+      
+      (sudo wget https://raw.githubusercontent.com/FalixNodes-Software/FalixNodes-Desktop/directive-rewrite-of-v4/src/base/etc/hosts) | zenity --progress --title="FalixNodes Desktop - Troubleshooting" --text="Downloading default hosts file..." --pulsate --width="500"
+      
+      zenity --info --text="Original hosts file was backed up to your Documents."
     `)
   }
 }
